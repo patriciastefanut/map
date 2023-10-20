7afbdac582354be867788e20c9653bcd88cbd763
patriciastefanut <147411989+patriciastefanut@users.noreply.github.com>
2023-10-20 13:25:04 +0300

    implement rest of the tests for BigNumbersArray

--- src/test/java/org/example/BigNumbersArrayTest.java
+++ src/test/java/org/example/BigNumbersArrayTest.java
@@ -11,6 +11,7 @@ public class BigNumbersArrayTest {
         int[] number1 = new int[]{3, 5, 0, 0, 0};
         int[] number2 = new int[]{1, 2, 0, 0, 0};
         int[] expected = new int[]{4, 7, 0, 0, 0};
+
         assertArrayEquals(expected, bigNumbersArray.addLargeNumbers(number1, number2));
     }
 
@@ -19,7 +20,7 @@ public class BigNumbersArrayTest {
         BigNumbersArray bigNumbersArray = new BigNumbersArray();
 
         assertThrows(Exception.class, () -> bigNumbersArray.addLargeNumbers(new int[]{}, new int[0]));
-        assertThrows(Exception.class, () -> bigNumbersArray.addLargeNumbers(new int[]{1,2}, new int[]{3}));
+        assertThrows(Exception.class, () -> bigNumbersArray.addLargeNumbers(new int[]{1, 2}, new int[]{3}));
     }
 
     @Test
@@ -28,6 +29,7 @@ public class BigNumbersArrayTest {
         int[] number1 = new int[]{3, 5, 0, 0, 0};
         int[] number2 = new int[]{1, 2, 0, 0, 0};
         int[] expected = new int[]{2, 3, 0, 0, 0};
+
         assertArrayEquals(expected, bigNumbersArray.subtractLargeNumbers(number1, number2));
     }
 
@@ -36,6 +38,37 @@ public class BigNumbersArrayTest {
         BigNumbersArray bigNumbersArray = new BigNumbersArray();
 
         assertThrows(Exception.class, () -> bigNumbersArray.subtractLargeNumbers(new int[]{}, new int[0]));
-        assertThrows(Exception.class, () -> bigNumbersArray.subtractLargeNumbers(new int[]{1,2}, new int[]{3}));
+        assertThrows(Exception.class, () -> bigNumbersArray.subtractLargeNumbers(new int[]{1, 2}, new int[]{3}));
+    }
+
+    @Test
+    public void expectedTestMultiplyLargeNumbers() throws Exception {
+        BigNumbersArray bigNumbersArray = new BigNumbersArray();
+        int[] number = new int[]{3, 5, 0, 0, 0};
+        int[] expected = new int[]{7, 0, 0, 0, 0};
+        assertArrayEquals(expected, bigNumbersArray.multiplyLargeNumberByDigit(number, 2));
+    }
+
+    @Test
+    public void unexpectedTestMultiplyLargeNumbers() {
+        BigNumbersArray bigNumbersArray = new BigNumbersArray();
+
+        assertThrows(Exception.class, () -> bigNumbersArray.divideLargeNumberByDigit(new int[]{}, 2));
+    }
+
+    @Test
+    public void expectedTestDivideLargeNumbers() throws Exception {
+        BigNumbersArray bigNumbersArray = new BigNumbersArray();
+        int[] number = new int[]{3, 5, 0, 0, 0};
+        int[] expected = new int[]{1, 7, 5, 0, 0};
+        assertArrayEquals(expected, bigNumbersArray.divideLargeNumberByDigit(number, 2));
+    }
+
+    @Test
+    public void unexpectedTestDivideLargeNumbers() {
+        BigNumbersArray bigNumbersArray = new BigNumbersArray();
+
+        assertThrows(Exception.class, () -> bigNumbersArray.divideLargeNumberByDigit(new int[]{}, 3));
+        assertThrows(Exception.class, () -> bigNumbersArray.divideLargeNumberByDigit(new int[]{1, 2}, 0));
     }
 }
